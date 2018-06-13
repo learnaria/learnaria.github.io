28f604a22851b78adc82e0a931b62fc891ca8281
Greg Gay <gregrgay@gmail.com>
2018-06-13 13:18:06 -0400

    add keyboard utilities

---
+++ assets/ik_utils.js
@@ -0,0 +1,31 @@
+var ik_utils = ik_utils || {};
+
+ik_utils.keys =  {
+	'tab': 9,
+	'enter': 13,
+	'esc': 27,
+	'space': 32,
+	'left': 37,
+	'up': 38,
+	'right': 39,
+	'down':  40
+}
+ik_utils.getTransitionEventName = function(){
+	var $elem, events, t, name;
+	
+	$elem = $('<div/>');
+	events = {
+		'transition': 'transitionend',
+		'OTransition': 'oTransitionEnd',
+		'MozTransition': 'transitionend',
+		'WebkitTransition': 'webkitTransitionEnd'
+	};
+	
+	for (t in events){
+		if ($elem.css(t) !== undefined){
+			name = events[t];
+		}
+	}
+	
+	return name;
+}
