502186141f06a06ee7d34b4f35a55db0c8e91d5e
Greg Gay <gregrgay@gmail.com>
2018-06-20 09:31:35 -0400

    comment out alerts, and update so only the first is read.

--- join.lib.js
+++ join.lib.js
@@ -246,7 +246,12 @@ JoinForm.prototype.validateForm = function () {
             error_count++;
             hasErrors = true;
             $s(fieldName + "_error").className = "error";
-            $s(fieldName + "_error").setAttribute("role", "alert");
+            // Uncomment the following if statement to add an ARIA alert to the error message
+            // Only the last alert is read, so limit alerts to the first error
+            // so it matches with focus sent to the first message
+            //if(error_count == 1){
+            //	$s(fieldName + "_error").setAttribute("role", "alert");
+            //}
             $s(fieldName + "_error").firstChild.nodeValue = error.message;
             if(error_count == 1){
             	$s(fieldName).focus();
