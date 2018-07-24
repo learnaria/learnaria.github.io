acc190bb8c92c830d0cb022255c3c22ac9bd82bb
Greg Gay <gregrgay@gmail.com>
2018-07-24 13:33:25 -0400

    added inaccessible progress bar, and .data() version as a workaround.

--- assets/ik_progressbar_data.js
+++ assets/ik_progressbar_data.js
@@ -2,6 +2,7 @@
 	
 	var pluginName = 'ik_progressbar',
 		defaults = { // values can be overitten by passing configuration options to plugin constructor 
+			'instructions': 'Press spacebar, or Enter to get progress',
 			'max': 100
 		};
 	
@@ -31,21 +32,59 @@
 		this.element
 			.attr({
 				'id': id,
+				'tabindex': -1, // add current element to tab oder
+				'aria-describedby': id + '_instructions' // add aria-describedby attribute 
 			})
 			.addClass('ik_progressbar')
-      ;
+			.on('keydown.ik', {'plugin': this}, this.onKeyDown);
 		
 		this.fill = $('<div/>')
 			.addClass('ik_fill');
 			
 		this.notification = $('<div/>') // add div element to be used to notify about the status of download
+			.attr({
+				'aria-role': 'region', // make it a live region
+				'aria-live': 'assertive', // set notofocation priority to high
+				'aria-atomic': 'additions' // notify only about newly added text
+			})
 			.addClass('ik_readersonly')
 			.appendTo(this.element);
-
+		
+		$('<div/>') // add div element to be used with aria-described attribute of the progressbar
+			.text(this.options.instructions) // get instruction text from plugin options
+			.addClass('ik_readersonly') // hide element from visual display
+			.attr({
+				'id': id + '_instructions', 
+				'aria-hidden': 'true'  // hide element from screen readers to prevent it from being read twice
+			})
+			.appendTo(this.element);
+			
 		$('<div/>')
 			.addClass('ik_track')
 			.append(this.fill)
 			.appendTo(this.element);
+		
+	};
+	
+	/** 
+	 * Handles kedown event on progressbar element. 
+	 *
+	 * @param {Object} event - Keyboard event.
+	 * @param {object} event.data - Event data.
+	 * @param {object} event.data.plugin - Reference to plugin.
+	 */
+	Plugin.prototype.onKeyDown = function(event) {
+		
+		switch(event.keyCode) {
+			
+			case ik_utils.keys.space:
+			case ik_utils.keys.enter:
+				event.preventDefault();
+				event.stopPropagation();
+				event.data.plugin.notify();
+				break;
+		}
+		
 		
 	};
 	
@@ -58,8 +97,8 @@
 		
 		var value;
 		
-		value = Number( this.element.data('value') ); // inaccessible
-		
+		value = Number( this.element.data('value') );
+				
 		return parseInt( value );
 		
 	};
@@ -99,10 +138,9 @@
 		}
 		
 		this.element
-			.data({ // inaccessible
+			.data({
 				'value': parseInt(val) 
-			}) 
-      ;
+			});
 		
 		this.updateDisplay();
 		
